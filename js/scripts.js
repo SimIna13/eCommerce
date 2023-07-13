326f166fbfa4eddcc8f6fbe6db014c0786ceb705
Simina B <simina.bejenari@student.usv.ro>
2023-07-13 13:43:25 +0300

    added the footer of the page, applied some styles to it and written the js code for getting the current year beside the Copyright

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+const currentYearElement = document.getElementById("current-year");
+currentYearElement.textContent = currentYear;
